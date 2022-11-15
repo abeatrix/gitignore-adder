f6a05946b3157ece06f06e5272b5e222422c94f6
Beatrix <68532117+abeatrix@users.noreply.github.com>
2022-11-15 05:31:19 -0800

    Release v1.0.0 (#4)
    * Add options to append and prepend to file
    
    * Update changelog for 1.0.0

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,24 +1,26 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-
+  const toAppend = vscode.workspace
+    .getConfiguration('gitignore-adder')
+    .get('append');
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "gitignore-adder.createFile",
+  let createFile = vscode.commands.registerCommand(
+    'gitignore-adder.creator',
     async () => {
       const rootDir = vscode.workspace.workspaceFolders
         ? vscode.workspace.workspaceFolders[0].uri.fsPath
         : null;
       const newFile = new vscode.WorkspaceEdit();
-      const filePath = vscode.Uri.file(rootDir + "/.gitignore");
+      const filePath = vscode.Uri.file(rootDir + '/.gitignore');
       // Create a .gitignore file if it doesn't exist
       newFile.createFile(filePath, { ignoreIfExists: true });
       await vscode.workspace.applyEdit(newFile);
@@ -26,37 +28,37 @@ export function activate(context: vscode.ExtensionContext) {
         .openTextDocument(filePath)
         .then((doc) => doc.save());
       // Display a message box to the user
-      vscode.window.showInformationMessage("Git Ignore has been created!");
+      vscode.window.showInformationMessage('Git Ignore has been created!');
     }
   );
 
-  let disposable2 = vscode.commands.registerCommand(
-    "gitignore-adder.addFile",
+  let addToFile = vscode.commands.registerCommand(
+    'gitignore-adder.adder',
     async (uri: vscode.Uri) => {
       const rootDir = vscode.workspace.workspaceFolders
         ? vscode.workspace.workspaceFolders[0].uri.fsPath
         : null;
       const newFile = new vscode.WorkspaceEdit();
-      const filePath = vscode.Uri.file(rootDir + "/.gitignore");
+      const filePath = vscode.Uri.file(rootDir + '/.gitignore');
       // Create a .gitignore file if it doesn't exist
       newFile.createFile(filePath, { ignoreIfExists: true });
-      const selected = uri.path.replace(`${rootDir}/`, "");
+      const selected = uri.path.replace(`${rootDir}/`, '');
+      const textDocument = await vscode.workspace.openTextDocument(filePath);
       await newFile.insert(
         filePath,
-        new vscode.Position(0, 0),
-        selected + "\n"
+        new vscode.Position(toAppend ? textDocument.lineCount + 1 : 0, 0),
+        selected + '\n'
       );
       await vscode.workspace.applyEdit(newFile);
-      const textDocument = await vscode.workspace.openTextDocument(filePath);
       await textDocument.save();
       // Display a message box to the user
       vscode.window.showInformationMessage(
-        "Added selected file to .gitignore!"
+        'Added selected file to .gitignore!'
       );
     }
   );
 
-  context.subscriptions.push(disposable, disposable2);
+  context.subscriptions.push(createFile, addToFile);
 }
 
 // this method is called when your extension is deactivated
